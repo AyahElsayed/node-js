78b9d1863f69df4bba93b741d46b8189783eb931
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-12-27 14:45:26 +0200

    routing

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
 // first server
 
 const http = require('http');
-const fs = require('fs') // to read files
+const fs = require('fs'); // to read files
+const url = require('url');
 
-const server = http.createServer((req , res)=>{
-  fs.readFile('./index.html', null, (error, data)=>{
-    if(error){
-      res.end('file not exist')
-    }else{
-      res.end(data)
-    }
-  })
-})
+const server = http.createServer((req, res) => {
 
-server.listen(3000 , '127.0.0.1' , ()=>{
+  const path = url.parse(req.url).pathname
+  console.log(path)
+
+  switch (path) {
+    case '/':
+      handleRequest('index.html', res)
+      break;
+
+    case '/any':
+      handleRequest('any.html', res)
+      break;
+
+    case '/user':
+      handleRequest('user.html', res)
+      break;
+
+    default:
+      res.end('this page not')
+      break;
+  }
+});
+
+
+server.listen(3000, '127.0.0.1', () => {
   console.log('server running...')
 })
 
-// to see the msg "hello in fisrt app" 
+function handleRequest(filepath, res) {
+  fs.readFile(filepath, null, (error, data) => {
+    if (error) {
+      res.end('file not exist')
+    } else {
+      res.end(data)
+    }
+  })
+}
+// to see the msg "hello in fisrt app"
 // go to the browser then 127.0.0.1:3000
